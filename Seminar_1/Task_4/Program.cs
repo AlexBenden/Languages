385edb369f5e798d30187477e74d9f8549fa7f75
Alex Savelev <bendenalex@gmail.com>
2024-03-13 18:16:41 +0300

    Add Seminar_1:Task_4v0.00

---
+++ Seminar_1/Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Написать программу которая на входе принимает одно число (N), а на выходе показывает все целые числа в промежутке -N до N
+// 4-> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+
+Console.WriteLine("Ведите число");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int negNum = num1 * -1;
+
+while(negNum<=num1)
+{
+    Console.Write(negNum+" ");
+    negNum = negNum + 1;
+}
