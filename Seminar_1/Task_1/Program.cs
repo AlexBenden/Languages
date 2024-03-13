2c036a833f4be726f607954fafe4827c56c08789
Alex Savelev <bendenalex@gmail.com>
2024-03-13 17:31:38 +0300

    Add Seminar_1:Task_1v0.00

---
+++ Seminar_1/Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу которая на вход принимает число и на выход выдает квадрат данного числа (число умноженное на самого себя)
+//Например:
+// 4 -> 16
+// -3 -> 9
+
+Console.WriteLine("Введите число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num * num;
+
+Console.WriteLine(result);
