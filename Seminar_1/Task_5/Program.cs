5c715d903bb7afe0d17ad68390f4af4dd6e93568
Alex Savelev <bendenalex@gmail.com>
2024-03-13 18:24:18 +0300

    Add Seminar_1:Task_5v0.00

---
+++ Seminar_1/Task_5/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу которая на входе принимает трехзначное число и на выходе показывает последнюю цифру этого числа
+// 456 -> 6
+
+Console.WriteLine("Ведите число");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int result = num1 %10;
+
+Console.WriteLine("Последнее число "+result);
